c5f01af66220029025aeed9f9dd87928413ba768
willrockies <wlsn.ads@gmail.com>
2024-05-09 15:33:06 -0300

    estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,40 @@
-import React from "react";
-import { Title } from './styles'
+import React, { useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
+import logoImg from "../../assets/logo.svg"
+
+import { Title, Form, Repositories } from './styles';
+
 const Dashboard: React.FC = () => {
+    
     return (
-        <Title>Explore repositórios no Github.</Title>
+        <>
+            <img src={logoImg} alt="Github explorer" />
+            <Title>Explore repositórios no Github.</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório"></input>
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars.githubusercontent.com/u/42125277?s=400&u=d95e89d21b5b95e9e2fba9d9c0fd997a9ec9deb9&v=4"
+                        alt="Wilson Alves da Silva" />
+                    <div>
+                        <strong>DDDTypescript</strong>
+                        <p>teste</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+
+            </Repositories>
+        </>
+
     )
 };
+
 // function Dashboard() {
 //     return (
 //         <>
@@ -12,4 +42,6 @@ const Dashboard: React.FC = () => {
 //         </>
 //     )
 // }
+
+
 export default Dashboard;
