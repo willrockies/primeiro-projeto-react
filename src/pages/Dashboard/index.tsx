8d8759e093b5c866bddf0d6f2c55f57503426318
willrockies <wlsn.ads@gmail.com>
2024-05-09 16:09:38 -0300

    Conectando API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,34 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import api from "../../services/api";
 import logoImg from "../../assets/logo.svg"
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+    full_name: string;
+    owner: {
+        login: string,
+        avatar_url: string;
+    };
+    description: string;
+}
+
 const Dashboard: React.FC = () => {
-    
+    const [newRepo, setNewRepo] = useState('')
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    async function handleAddRepository (event: FormEvent<HTMLFormElement>) :Promise<void> {
+        // Adição de um novo repositório
+        // Consumir API do Github
+        // Salvar novo repositório no estado
+        event.preventDefault();
+
+        const response = await api.get<Repository>(`repos/${newRepo}`);
+        const repository = response.data;
+        setRepositories([...repositories, repository]);
+        setNewRepo('');
+        console.log(response.data);
+    }
     return (
         <>
             <img src={logoImg} alt="Github explorer" />
             <Title>Explore repositórios no Github.</Title>
 
-            <Form>
-                <input placeholder="Digite o nome do repositório"></input>
+            <Form onSubmit={handleAddRepository}>
+                <input 
+                value={newRepo}
+                onChange={(e) => setNewRepo(e.target.value)}
+                placeholder="Digite o nome do repositório"></input>
                 <button type="submit">Pesquisar</button>
             </Form>
 
             <Repositories>
-                <a href="teste">
-                    <img src="https://avatars.githubusercontent.com/u/42125277?s=400&u=d95e89d21b5b95e9e2fba9d9c0fd997a9ec9deb9&v=4"
-                        alt="Wilson Alves da Silva" />
+                {repositories.map(repository => (
+                    <a key={repository.full_name} href="teste">
+                    <img src={repository.owner.avatar_url}
+                        alt={repository.owner.login} />
                     <div>
-                        <strong>DDDTypescript</strong>
-                        <p>teste</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
 
                     <FiChevronRight size={20} />
                 </a>
 
+                ))}
             </Repositories>
         </>
 
