c007a288e39f8884304247c225786b0ba4d3a294
willrockies <wlsn.ads@gmail.com>
2024-05-10 11:09:44 -0300

    salvando no storage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, useEffect } from "react";
 import { FiChevronRight } from "react-icons/fi";
 import api from "../../services/api";
 import logoImg from "../../assets/logo.svg"
@@ -17,9 +17,22 @@ interface Repository {
 const Dashboard: React.FC = () => {
     const [newRepo, setNewRepo] = useState('');
     const [inputError, setInputError] = useState('');
-    const [repositories, setRepositories] = useState<Repository[]>([]);
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storagedRepositories = localStorage.getItem(
+            "@GithubExplorer:repositories",
+        );
+    
+        if(storagedRepositories){
+            return JSON.parse(storagedRepositories);
+        }
+        return [];
+    });
 
-
+    
+    
+    useEffect(() => {
+        localStorage.setItem("@GithubExplorer:repositories", JSON.stringify(repositories)); // @GithubExplorer:repositories é para evitar conflitos caso outras aplicações esta rodando na mesma porta 
+    }, [repositories]);
 
     async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
         // Adição de um novo repositório
